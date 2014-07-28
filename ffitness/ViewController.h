52bd8d7d6f93e42bde40b14bf7850a3ba39b5a6a
andriitishchenko <andrii.tishchenko@gmail.com>
2014-07-28 22:53:47 +0300

    Initial Commit

---
+++ ffitness/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ffitness
+//
+//  Created by AndruX on 7/28/14.
+//  Copyright (c) 2014 AndruX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
